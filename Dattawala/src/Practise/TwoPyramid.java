d93548d054bfe21bb2c1ed75c996186f5333c685
rahul <rahul@LAPTOP-LFPBBS7T>
2020-06-08 02:00:55 +0530

    for loop, 2. Two pyramid

---
+++ Dattawala/src/Practise/TwoPyramid.java
@@ -0,0 +1,35 @@
+package Practise;
+
+import java.util.Scanner;
+
+public class TwoPyramid {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the number of pyraminds required : ");
+
+		int NoOfPyrmids = input.nextInt();
+
+		for(int k = 1; k <= NoOfPyrmids ; k++) {
+			
+		
+		for (int i = 0; i <= 10; i++) {
+
+			for (int j = 10; j >= i; j--) {
+
+				System.out.print(" ");
+			}
+
+			for (int j = 0; j <= i; j++) {
+
+				System.out.print(" *");
+			}
+			
+			System.out.println();
+		}
+		}
+	}
+
+}
