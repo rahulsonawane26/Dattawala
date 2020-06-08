2e1c098cd69524aa1c44246580631b8652bdb6ad
rahul <rahul@LAPTOP-LFPBBS7T>
2020-06-08 23:12:05 +0530

    Leap YEar

---
+++ Dattawala/src/Practise/LeapYear.java
@@ -0,0 +1,19 @@
+package Practise;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+
+		int year = 1901;
+
+		for (int i = year; i <= 2020; i++) {
+
+			if (year % 4 == 0 && (year%100 != 0 || year%400== 0 )) {
+				System.out.println(year);
+				
+			}
+			year++;
+		}
+
+	}
+}
